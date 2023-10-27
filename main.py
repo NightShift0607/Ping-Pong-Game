1bce1d9ed9ef19bbf7488b2fec56eb20dedc3542
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2023-10-27 15:17:17 +0530

    1st paddle created

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800,height=600)
 screen.title("Ping Pong Game")
+screen.tracer(0)
 
 
+paddle = Turtle("square")
+paddle.setheading(90)
+paddle.penup()
+paddle.color("white")
+paddle.shapesize(stretch_len=5,stretch_wid=1)
+paddle.goto(350,0)
+
+def up():
+    paddle.forward(20)
+
+def down():
+    paddle.backward(20)
+
+screen.listen()
+screen.onkey(up,"Up")
+screen.onkey(down,"Down")
+
+flag = True
+while flag:
+    screen.update()
 
 screen.exitonclick()
