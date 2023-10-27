eb0556bbf7f78d4e3bf2435326768f529efa1c5c
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2023-10-27 19:41:19 +0530

    creation of paddle class

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
 
 
+# screen setup
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800,height=600)
 screen.title("Ping Pong Game")
 screen.tracer(0)
 
+# paddle creation
+r_paddle = Paddle(350,0)
+l_paddle = Paddle(-350,0)
 
-paddle = Turtle("square")
-paddle.setheading(90)
-paddle.penup()
-paddle.color("white")
-paddle.shapesize(stretch_len=5,stretch_wid=1)
-paddle.goto(350,0)
-
-def up():
-    paddle.forward(20)
-
-def down():
-    paddle.backward(20)
-
+# paddle movement
 screen.listen()
-screen.onkey(up,"Up")
-screen.onkey(down,"Down")
+screen.onkeypress(r_paddle.up,"Up")
+screen.onkeypress(r_paddle.down,"Down")
+screen.onkeypress(l_paddle.up,"w")
+screen.onkeypress(l_paddle.down,"s")
 
+
+# gameplay control
 flag = True
 while flag:
     screen.update()
+
 
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+class Paddle:
+    def __init__(self,xcor,ycor):
+        self.xcor = xcor
+        self.ycor = ycor
+        self.paddle = Turtle("square")
+        self.create_paddle()
+    
+    def create_paddle(self):
+        self.paddle.setheading(90)
+        self.paddle.penup()
+        self.paddle.color("white")
+        self.paddle.shapesize(stretch_len=5,stretch_wid=1)
+        self.paddle.goto(self.xcor,self.ycor)
+    
+    def up(self):
+        self.paddle.forward(20)
+    
+    def down(self):
+        self.paddle.backward(20)
